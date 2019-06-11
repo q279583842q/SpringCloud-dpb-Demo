69856c216a90274edb9c51928cef6a2f4c2df80b
dpb <dengpbs@163.com>
2019-06-11 21:57:50 +0800

    hystrix 请求合并案例

---
+++ springcloud-eureka-ribbon-consumer-batch/src/main/java/com/dpb/spring_cloud_eureka_consumer/pojo/User.java
@@ -0,0 +1,46 @@
+package com.dpb.spring_cloud_eureka_consumer.pojo;
+
+/**
+ * @program: springcloud-eureka-provider
+ * @description: UserBean
+ * @author: 波波烤鸭
+ * @create: 2019-05-28 17:31
+ */
+public class User {
+    private int userid;
+    private String username;
+    private int userage;
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getUserage() {
+        return userage;
+    }
+
+    public void setUserage(int userage) {
+        this.userage = userage;
+    }
+
+    public User(int userid, String username, int userage) {
+        this.userid = userid;
+        this.username = username;
+        this.userage = userage;
+    }
+
+    public User() {
+    }
+}
---
+++ springcloud-eureka-ribbon-consumer-batch/src/main/java/com/dpb/spring_cloud_eureka_consumer/service/UserService.java
@@ -0,0 +1,63 @@
+package com.dpb.spring_cloud_eureka_consumer.service;
+
+import com.dpb.spring_cloud_eureka_consumer.pojo.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: springcloud-eureka-consumer
+ * @description: 用户的业务处理
+ * @author: 波波烤鸭
+ * @create: 2019-05-28 17:43
+ */
+@Service
+public class UserService {
+
+    /**
+     * ribbon 负载均衡
+     *    LoadBalancerClient 通过服务名称可以获取对应的服务的相关信息 ip port等
+     */
+    @Autowired
+   private LoadBalancerClient loadBalancerClient;
+
+    @HystrixCommand(fallbackMethod = "fallBack")
+    public List<User> getUsers(){
+        // ServiceInstance 封装的有服务的基本信息  IP和端口等
+        ServiceInstance si = this.loadBalancerClient.choose("eureka-ribbon-provider");
+        StringBuilder sb = new StringBuilder();
+        sb.append("http://")
+                .append(si.getHost())
+                .append(":")
+                .append(si.getPort())
+                .append("/user");
+        System.out.println("服务地址:"+sb.toString());
+        // SpringMVC RestTemplate
+        RestTemplate rt = new RestTemplate();
+        ParameterizedTypeReference<List<User>> type = new ParameterizedTypeReference<List<User>>() {};
+        // ResponseEntity:封装了返回值的信息
+        ResponseEntity<List<User>> response = rt.exchange(sb.toString(), HttpMethod.GET,null,type);
+        List<User> list = response.getBody();
+        return list;
+    }
+
+    /**
+     * 服务降级
+     *   返回托底数据的方法
+     * @return
+     */
+    public List<User> fallBack(){
+        List<User> list = new ArrayList<>();
+        list.add(new User(3,"我是托底数据",22));
+        return list;
+    }
+}
