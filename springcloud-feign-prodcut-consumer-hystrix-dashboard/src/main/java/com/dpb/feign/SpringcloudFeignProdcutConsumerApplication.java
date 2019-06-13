5ff5d4277ea49893e7c8edd70636d53a19db3a53
dpb <dengpbs@163.com>
2019-06-13 19:01:17 +0800

    dashboard 案例

--- springcloud-feign-prodcut-consumer-hystrix-dashboard/src/main/java/com/dpb/feign/SpringcloudFeignProdcutConsumerApplication.java
+++ springcloud-feign-prodcut-consumer-hystrix-dashboard/src/main/java/com/dpb/feign/SpringcloudFeignProdcutConsumerApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
 public class SpringcloudFeignProdcutConsumerApplication {
 
     public static void main(String[] args) {
